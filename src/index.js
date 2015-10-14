900fc2cf58107a843982503f447e6b78a6cce6ab
Michele Memoli <michele@100shapes.com>
2015-10-14 15:03:52 +0100

    Updated collectionID

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import WallContainer from './components/WallContainer';
 import './style/main.scss';
 
 ReactDOM.render(
-    <WallContainer collectionId="561ba63445284e1740e016f7" limit="2"/>,
+    <WallContainer collectionId="561e601745284e15a8d5343d" limit="10"/>,
     document.getElementById('root')
 );
