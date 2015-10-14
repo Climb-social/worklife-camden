f7cb6525e0c0ed372bf3f29852e7854d3978ee06
Michele Memoli <michele@100shapes.com>
2015-10-14 23:09:45 +0100

    Using react-climb-social

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WallContainer from './components/WallContainer';
+import {WallContainer} from 'react-climb-social';
 
 import './style/main.scss';
 
 ReactDOM.render(
-    <WallContainer collectionId="561e601745284e15a8d5343d" limit="3"/>,
+    <WallContainer collectionId="561e601745284e15a8d5343d" limit="6"/>,
     document.getElementById('root')
 );
