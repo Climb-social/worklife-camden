136a1e182907022d1e69dc3393a1d55e49231baa
Michele Memoli <michele@100shapes.com>
2015-10-14 11:52:38 +0100

    Added webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by michele on 14/10/15.
+ */
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    debug: true,
+    context: path.resolve(__dirname),
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'worklife-camden.js'
+    },
+    resolve: {
+        root: path.resolve(__dirname),
+        extensions: ['', '.js', '.jsx'],
+        modulesDirectories: ['node_modules']
+    },
+    node: {
+        fs: 'empty'
+    },
+    eslint: {
+        configFile: path.join(__dirname, '.eslintrc')
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'eslint'
+            }
+        ],
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {stage: 0}
+            }
+        ]
+    },
+    externals: {
+        'react': 'React'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: 'src/index.html'
+        })
+    ]
+};
