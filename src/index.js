308c9f87b264a84491f0047d371bd3b30eed9f4d
Michele Memoli <michele@100shapes.com>
2015-10-15 16:02:34 +0100

    Switched collectionId

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import {WallContainer} from 'react-climb-social';
 import './style/main.scss';
 
 ReactDOM.render(
-    <WallContainer collectionId="561e601745284e15a8d5343d" limit="8"/>,
+    <WallContainer collectionId="561fbfce45284e041471e8f7" limit="8"/>,
     document.getElementById('root')
 );
